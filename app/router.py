8566cf88f4495ea848152e67641aaf5dc03ca1de
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-12-26 01:29:13 +0530

    implement retrieve user by id route function in router.py

--- app/router.py
+++ app/router.py
@@ -38,6 +38,11 @@ async def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_d
     _users = crud.get_users(db, skip, limit)
     return Response(status="Ok", code="200", message="Success fetch all data", result=_users)
 
+@router.get("/{id}")
+async def get_user_by_id(id: int, db: Session = Depends(get_db)):
+    _user = crud.get_user_by_id(db, user_id=id)
+    return Response(status="Ok", code="200", message="Success fetch data", result=_user).dict(exclude_none=True)
+
 @router.post("/create")
 async def create_user(request: RequestUser, db: Session = Depends(get_db)):
     """
