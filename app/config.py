85f5c54dc6529f10eed7d48a1020fccab2a0f000
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-12-25 23:35:37 +0530

    refactor db config

---
+++ app/config.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/python_db'
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+"""
+This module provides the configuration settings for the application.
+
+- `DATABASE_URL`: The URL of the SQLite database.
+- `engine`: The SQLAlchemy engine object for connecting to the database.
+- `SessionLocal`: The sessionmaker object for creating database sessions.
+- `Base`: The declarative base class for defining database models.
+"""
