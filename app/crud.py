0b05120a13a142401e0a23db108808aa9ad5f047
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-12-26 00:09:35 +0530

    implement retrieve user by id function in crud.py

--- app/crud.py
+++ app/crud.py
@@ -15,3 +15,16 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
         List[User]: A list of User objects.
     """
     return db.query(User).offset(skip).limit(limit).all()
+
+def get_user_by_id(db: Session, user_id: int):
+    """
+    Retrieve a user from the database by their ID.
+
+    Args:
+        db (Session): The database session.
+        user_id (int): The ID of the user to retrieve.
+
+    Returns:
+        User: The user object if found, None otherwise.
+    """
+    return db.query(User).filter(User.id == user_id).first()
